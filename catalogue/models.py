0fbf8c87ce30907a1e2e2be42f2954ce299526c2
sakhuy <dwisakha244@gmail.com>
2023-10-25 16:11:43 +0700

    ini hanya percobaan

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    isbn = models.CharField(max_length=13, unique=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    year_of_publication = models.PositiveIntegerField()
+    publisher = models.CharField(max_length=50)
+    image_url_large = models.URLField()
+    image_url_medium = models.URLField()
+
+    def __str__(self):
+        return self.title
