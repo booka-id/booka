d6a411e8fb864891c32d07d5f2fd9acd2c9082f7
syrfmy <syariefmulyadi24@gmail.com>
2023-10-29 10:12:14 +0700

    alter required fields

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from book.views import get_books
+from book.views import get_books, get_books_by_title
 
 app_name = "book"
 
 urlpatterns = [
     path("", get_books, name="get_books"),
+    path("book_by_title/", get_books_by_title, name='book_by_title')
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 from book.models import Book
 
@@ -9,3 +11,19 @@ def get_books(request):
     data = Book.objects.all()
     return HttpResponse(serializers.serialize("json", data),
     content_type = "application/json")
+
+@csrf_exempt
+def get_books_by_title(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            title = data.get('title', '')  # Extract the 'title' parameter
+
+            # Perform your query based on 'title'
+            data = Book.objects.filter(title__icontains=title)[:5]
+
+            return JsonResponse(list(data.values()), safe=False, content_type="application/json")
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400, content_type="application/json")
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400, content_type="application/json")
