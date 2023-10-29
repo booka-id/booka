ed75802a334a98b15d22ad4c60bddb609ece9bf7
sakhuy <dwisakha244@gmail.com>
2023-10-29 22:29:54 +0700

    tespush

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from catalogue.views import show_catalogue, add_book, show_json, detail_book, buy_book, delete_book, edit_book
+from catalogue.views import show_catalogue, add_book, show_json, detail_book, buy_book, delete_book, edit_book, add_book_ajax, get_book_json, get_bookstock_json, search_book, pay_book
 
 app_name = 'catalogue'
 
 urlpatterns = [
     path('', show_catalogue, name="show_catalogue"),
-    path('add_book/', add_book, name='add_book'),
+    path('add-book/', add_book, name='add_book'),
+    path('add-book-ajax/', add_book_ajax, name='add_book_ajax'),
     path('json/', show_json, name="show_json"),
     path('book/<int:pk>', detail_book, name='detail_book'),
     path('book/<int:pk>/purchase', buy_book, name="buy_book"),
     path('book/<int:pk>/edit/', edit_book, name='edit_book'),
     path('book/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('get-book/', get_book_json, name='get_book_json'),
+    path('get-bookstock/', get_bookstock_json, name='get_bookstock_json'),
+    path('search_book/', search_book, name='search_book'),
+    path('pay_book/<int:pk>/', pay_book, name='pay_book'),
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from django.urls import reverse
 from book.models import Book
@@ -9,6 +10,7 @@ from catalogue.models import BookStock
 from catalogue.forms import BookForm, BookStockForm, AddBookForm
 
 # Create your views here.
+
 def show_catalogue(request):
     books = Book.objects.all()
     b = BookStock.objects.all()
@@ -16,8 +18,13 @@ def show_catalogue(request):
         'books': books,
         'tes' : b
     }
+    if request.user.is_superuser:
+        return render(request, "admin/catalogue_admin.html", context)
+    
+    else:
+        return render(request, "user/catalogue_user.html", context)
 
-    return render(request, "admin/catalogue_admin.html", context)
+
 
 def add_book(request):
     if request.method == 'POST':
@@ -38,6 +45,28 @@ def add_book(request):
         form = AddBookForm()
 
     return render(request, 'admin/add_book.html', {'form': form})
+
+def add_book_ajax(request):
+    if request.method == 'POST':
+        isbn = request.POST.get("isbn")
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        year = request.POST.get("year")
+        publisher = request.POST.get("publisher")
+        image_url_small = request.POST.get("image_url_small")
+        image_url_medium = request.POST.get("image_url_medium")
+        image_url_large = request.POST.get("image_url_large")
+        quantity = request.POST.get("quantit+y")
+        price = request.POST.get("price")
+
+        new_book = Book(isbn=isbn, title=title, author=author, year=year, publisher=publisher, image_url_small=image_url_small, image_url_medium=image_url_medium, image_url_large=image_url_large)
+        new_book.save()
+        new_book_stock = BookStock(book=new_book, quantity=quantity, price=price)
+        new_book_stock.save()
+
+        return HttpResponse(b"CREATED", status=201)
+    
+    return HttpResponseNotFound()
     # book_form = BookForm(request.POST or None)
     # book_stock_form = BookStockForm(request.POST or None)
 
@@ -60,11 +89,23 @@ def detail_book(request, pk):
     context = {'book': book}
     return render(request, 'detail_book.html', context)
 
+@login_required(login_url='/login')
 def buy_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
+    book = get_object_or_404(BookStock, pk=pk)
     context= {'book' : book}
     return render(request, 'user/payment.html', context)
 
+@login_required(login_url='/login')
+def pay_book(request, pk):
+    book = get_object_or_404(BookStock, pk=pk)
+    if request.method == 'POST':
+        payment_method = request.POST.get('payment_method')
+        amount = 1
+        book.quantity -= amount
+        book.save()
+        return redirect('catalogue:show_catalogue')
+
+@login_required(login_url='/login')
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book.delete()
@@ -76,6 +117,7 @@ def show_json(request):
     data = BookStock.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+@login_required(login_url='/login')
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book_stock = get_object_or_404(BookStock,pk=pk)
@@ -96,3 +138,19 @@ def edit_book(request, pk):
     context = {'book_form': book_form, 'book_stock_form': book_stock_form}
     
     return render(request, 'admin/edit_book.html', context)
+
+def get_book_json(request):
+    book_item = Book.objects.all()
+    return HttpResponse(serializers.serialize('json', book_item))
+
+def get_bookstock_json(request):
+    bookstock_item = BookStock.objects.all()
+    return HttpResponse(serializers.serialize('json', bookstock_item))
+
+def search_book(request):
+    if 'search_query' in request.GET:
+        search_query = request.GET['search_query']
+        results = list(Book.objects.filter(title__icontains=search_query).values('pk', 'title', 'author', 'image_url_large'))
+        return JsonResponse(results, safe=False)
+
+    return JsonResponse([], safe=False)
